b8aa9e2418f8ca37ee4dd693f25910a856d00faf
kmishita <kmishita.16@gmail.com>
2019-08-28 09:43:19 +0530

    Database Migration

---
+++ src/PalTracker/TimeEntryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PalTracker
+{
+    public class TimeEntryContext : DbContext
+    {
+        public TimeEntryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TimeEntryRecord> TimeEntryRecords { get; set; }
+
+    }
+}
---
+++ src/PalTracker/TimeEntryRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PalTracker
+{
+    [Table("time_entries")]
+    public class TimeEntryRecord
+    {
+        [Column("id")]
+        public long? Id { get; set; }
+
+        [Column("project_id")]
+        public long ProjectId { get; set; }
+
+        [Column("user_id")]
+        public long UserId { get; set; }
+
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        [Column("hours")]
+        public int Hours { get; set; }
+        public TimeEntryRecord ()
+        {
+
+        }
+    }
+}
